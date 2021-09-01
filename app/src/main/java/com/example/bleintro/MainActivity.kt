872e386b0d1b6cbc4504245f01f8ed7e426f5445
itzkou <elbehi.koutheir@gmail.com>
2021-09-01 15:46:55 +0100

    init commit

--- app/src/main/java/com/example/bleintro/MainActivity.kt
+++ app/src/main/java/com/example/bleintro/MainActivity.kt
@@ -4,34 +4,36 @@ import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.bleintro.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val permissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            // Do something if permission granted
             if (isGranted) {
-                Snackbar.make(this, binding.root, "Make sure you enable your location to start scanning", Snackbar.LENGTH_SHORT)
-                    .show()
-                Log.i("DEBUG", "permission granted")
+                startBleScan()
             } else {
-                Snackbar.make(this, binding.root, "Location denied...", Snackbar.LENGTH_SHORT)
-                    .show()
-                Log.i("DEBUG", "permission denied")
+                Toast.makeText(this, "Location denied...", Toast.LENGTH_SHORT).show()
             }
         }
     private val enableBluetooth =
@@ -43,6 +45,28 @@ class MainActivity : AppCompatActivity() {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothManager.adapter
     }
+   /* private val isGpsEnabled: Boolean by lazy {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.isLocationEnabled
+
+    }*/
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            with(result.device) {
+                Log.i(
+                    "“ScanCallback”",
+                    "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address"
+                )
+            }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,9 +81,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         promptEnableBluetooth()
-        startBleScan()
+        //promptEnableGps()
+        setupUi()
 
     }
+
+
 
 
     private fun promptEnableBluetooth() {
@@ -69,31 +96,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startBleScan() {
+   /* private fun promptEnableGps() {
+        if (!isGpsEnabled)
+            Toast.makeText(this, "Please enable location", Toast.LENGTH_SHORT).show()
+
+    }*/
+
+    private fun setupUi() {
+        /** checking permissions **/
         binding.btnScan.setOnClickListener {
             checkLocationPermission()
         }
-
     }
 
     private fun checkLocationPermission() {
-        //todo add android sdk < M permissions and handle multiple permissions
+        //todo add android sdk < M permissions and handle multiple permissions ( add location )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             when {
                 ContextCompat.checkSelfPermission(
                     this,
                     Manifest.permission.ACCESS_FINE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    // You can use the API that requires the permission.
-                    Snackbar.make(this, binding.root, "Make sure you enable your location to start scanning", Snackbar.LENGTH_SHORT)
-                        .show()
+                    // You can use the permission.
+                    startBleScan()
                 }
                 shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                    // In an educational UI, explain to the user why your app requires this
-                    // permission for a specific feature to behave as expected. In this UI,
-                    // include a "cancel" or "no thanks" button that allows the user to
-                    // continue using your app without granting the permission.
-                    //showInContextUI(...)
+                    // explain why this permission is needed using UI
                     Snackbar.make(
                         this,
                         binding.root,
@@ -105,7 +133,7 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 else -> {
-                    // You can directly ask for the permission.
+                    // directly ask for the permission.
                     // The registered ActivityResultCallback gets the result of this request.
                     permissionLauncher.launch(
                         Manifest.permission.ACCESS_FINE_LOCATION
@@ -116,11 +144,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    private fun startBleScan() {
+        bleScanner.startScan(null, scanSettings, scanCallback)
+     }
+
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+     }
+
     private fun goToSettings() {
         val myAppSettings = Intent(
-            Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse(
-                "package:$packageName"
-            )
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:$packageName")
         )
         myAppSettings.addCategory(Intent.CATEGORY_DEFAULT)
         myAppSettings.flags = Intent.FLAG_ACTIVITY_NEW_TASK
